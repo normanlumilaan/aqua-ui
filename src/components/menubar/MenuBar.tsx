3d959295a5f22acc31410d2f5b7d4435a50fe133
Norman Lumilaan <norman.lumilaan@gmail.com>
2024-09-08 02:25:30 +0300

    refactor: add columns to menubar

--- src/components/menubar/MenuBar.tsx
+++ src/components/menubar/MenuBar.tsx
@@ -27,52 +27,56 @@ export const MenuBar: React.FC = () => {
   return (
     <header className="aqua-menubar">
       <div className="aqua-menubar__row">
-        <div className="aqua-menubar__item">
-          <a href="/" className="aqua-menubar__item-a" id="aqua-menubar-logo">
-            <figure className="icon">
-              <img src={logo} alt="heart" width="24" height="24" />
-            </figure>
-          </a>
-          <MenuBarSubmenu id="aqua-menubar-program-menu">
-            {programMenu.map(({ url, label }) => (
-              <MenuBarSubmenuItem key={label} label={label} url={url} />
-            ))}
-          </MenuBarSubmenu>
+        <div className="aqua-menubar__col">
+          <div className="aqua-menubar__item">
+            <a href="/" className="aqua-menubar__item-a" id="aqua-menubar-logo">
+              <figure className="icon">
+                <img src={logo} alt="heart" width="24" height="24" />
+              </figure>
+            </a>
+            <MenuBarSubmenu id="aqua-menubar-program-menu">
+              {programMenu.map(({ url, label }) => (
+                <MenuBarSubmenuItem key={label} label={label} url={url} />
+              ))}
+            </MenuBarSubmenu>
+          </div>
+          <div className="aqua-menubar__item aqua-menubar__item--programname">
+            <button className="aqua-menubar__item-a" type="button">
+              Aqua UI
+            </button>
+            <MenuBarSubmenu id="aqua-menubar-program-menu">
+              {sampleSubmenu.map(({ url, label, shortcut }) => (
+                <MenuBarSubmenuItem
+                  key={label}
+                  label={label}
+                  url={url}
+                  shortcut={shortcut}
+                />
+              ))}
+            </MenuBarSubmenu>
+          </div>
+          <nav className="aqua-menubar__nav">
+            <ul className="aqua-menubar__nav-menu">
+              <li className="aqua-menubar__item">
+                <button className="aqua-menubar__item-a">File</button>
+              </li>
+              <li className="aqua-menubar__item">
+                <a href="#" className="aqua-menubar__item-a">
+                  Edit
+                </a>
+              </li>
+              <li className="aqua-menubar__item">
+                <a href="#" className="aqua-menubar__item-a">
+                  Info
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <div className="aqua-menubar__item aqua-menubar__item--programname">
-          <button className="aqua-menubar__item-a" type="button">
-            Aqua UI
-          </button>
-          <MenuBarSubmenu id="aqua-menubar-program-menu">
-            {sampleSubmenu.map(({ url, label, shortcut }) => (
-              <MenuBarSubmenuItem
-                key={label}
-                label={label}
-                url={url}
-                shortcut={shortcut}
-              />
-            ))}
-          </MenuBarSubmenu>
-        </div>
-        <nav className="aqua-menubar__nav">
-          <ul className="aqua-menubar__nav-menu">
-            <li className="aqua-menubar__item">
-              <button className="aqua-menubar__item-a">File</button>
-            </li>
-            <li className="aqua-menubar__item">
-              <a href="#" className="aqua-menubar__item-a">
-                Edit
-              </a>
-            </li>
-            <li className="aqua-menubar__item">
-              <a href="#" className="aqua-menubar__item-a">
-                Info
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="aqua-menubar__item aqua-menubar__item--datetime">
-          <DateTime />
+        <div className="aqua-menubar__col--right">
+          <div className="aqua-menubar__item aqua-menubar__item--datetime">
+            <DateTime />
+          </div>
         </div>
       </div>
     </header>
