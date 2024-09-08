d63c5f1c467a9cf399d34aa70d6de13651f64066
Norman Lumilaan <norman.lumilaan@gmail.com>
2024-09-08 18:16:22 +0300

    refactor: update window interface

--- src/components/window/Window.tsx
+++ src/components/window/Window.tsx
@@ -2,14 +2,24 @@ import React, { type HTMLAttributes } from 'react'
 import clsx from 'clsx'
 
 export interface WindowProps extends HTMLAttributes<HTMLDivElement> {
-  /** Window has focus */
-  focus: boolean
+  /** Window id */
+  id: string
+  /** Window label (visible title) */
+  label: string
+  /** Position x-axis */
+  x: number
+  /** Position y-axis */
+  y: number
+  /** focus
+   * @deprecated
+   */
+  focus?: boolean
 }
 
 /*
  * Window component that holds window-title and window-body
  */
-export const Window: React.FC<WindowProps> = ({
+export const Window: React.FC<WindowProps & HTMLAttributes<HTMLDivElement>> = ({
   children,
   className,
   focus = false,
